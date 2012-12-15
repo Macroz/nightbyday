af745c61ad24dfa07c51fa943e251b0c182d68b5
Markku Rontu <markku.rontu@iki.fi>
2012-12-15 13:17:57 +0200

    Basic setup for heroku

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Night By Day, Ludum Dare #25 game programming competition entry by @zorcam"
   :url "http://nightbyday.heroku.com"
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ring/ring-jetty-adapter "1.1.0"]
                  [crate "0.2.1"]
                  [enfocus "1.0.0-beta2"]
                  [compojure "1.1.1"]
---
+++ src/nightbyday/server.clj
@@ -0,0 +1,34 @@
+(ns nightbyday.server
+  (:use compojure.core)
+  (:require [compojure.route :as route])
+  (:use [ring.adapter.jetty :only [run-jetty]])
+  (:use [hiccup core element page]))
+
+(defn welcome-page []
+  (html5 [:html
+          [:head]
+          [:body
+           [:h1 "Night By Day"]
+           [:a {:href "/game"} "Play"]]]))
+
+(defn game-page []
+  (html5 [:html
+          [:head
+           (include-js "js/raphael-min.js")
+           (include-js "js/cljs.js")]
+          [:body {:onload "nightbyday.main.startup();"}
+           [:canvas#canvas]]]))
+
+(defroutes handler
+  (GET "/" [] (welcome-page))
+  (GET "/game" [] (game-page))
+  (route/resources "/")
+  (route/not-found "Page not found!"))
+
+(def app (-> handler))
+
+(defn- run []
+  (run-jetty #'app {:port 8080 :join? false}))
+
+(defn -main [port]
+  (run-jetty #'app {:port (Integer. port)}))
