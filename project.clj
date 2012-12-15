5d0c31e19db20ee4ad384ef5b82c331f038b2f33
Markku Rontu <markku.rontu@iki.fi>
2012-12-15 11:24:46 +0200

    Initial version

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject nightbyday "0.1.0-SNAPSHOT"
+  :description "Night By Day, Ludum Dare #25 game programming competition entry by @zorcam"
+  :url "http://nightbyday.heroku.com"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [crate "0.2.1"]
+                 [enfocus "1.0.0-beta2"]
+                 [compojure "1.1.1"]
+                 [hiccup "1.0.1"]
+                 [ring "1.1.6"]
+                 [cheshire "3.0.0"]
+                 ]
+  :plugins [[lein-cljsbuild "0.2.7"]]
+  :cljsbuild {
+              :builds [{
+                        :source-path "src"
+                        :compiler {
+                                   :output-to "resources/public/js/cljs.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
